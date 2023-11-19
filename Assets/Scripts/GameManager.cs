7853c09c7f02676048a1a1b24dbca75570f966bb
Arke011 <146362737+Arke011@users.noreply.github.com>
2023-11-19 17:50:37 +0200

    idk what im doing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,14 +6,50 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public int hp;
+    public int maxHp = 3; // Set the maximum HP here
     public int currentLevel;
     public List<string> Levels;
     bool hasWon;
-
+    float targetTransitionScale;
+    public Transform transition;
+    public GameObject playerPrefab; // Prefab for the player
 
     void Start()
     {
         DontDestroyOnLoad(gameObject);
+        ResetGame();
+    }
+
+    void Update()
+    {
+        transition.localScale = Vector3.MoveTowards(transition.localScale,
+            Vector3.one * targetTransitionScale,
+            60 * Time.deltaTime);
+
+        if (hp <= 0)
+        {
+            // Player has lost all HP, restart from level 1
+            currentLevel = 0;
+            ResetGame();
+        }
+    }
+
+    void ResetGame()
+    {
+        hasWon = false;
+        hp = maxHp;
+        targetTransitionScale = 0;
+
+        if (playerPrefab != null)
+        {
+            GameObject oldPlayer = GameObject.FindGameObjectWithTag("Player");
+            if (oldPlayer != null)
+            {
+                Destroy(oldPlayer);
+            }
+
+            Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
+        }
     }
 
     public void Win()
@@ -21,19 +57,27 @@ public class GameManager : MonoBehaviour
         if (hasWon) return;
         currentLevel++;
         hasWon = true;
+        targetTransitionScale = 60;
         Invoke("LoadNextScene", 1f);
-        
     }
 
     void LoadNextScene()
     {
-        var lvlname = Levels[currentLevel];
-        SceneManager.LoadScene(lvlname);
-        hasWon = false;
+        if (currentLevel < Levels.Count)
+        {
+            SceneManager.LoadScene(Levels[currentLevel]);
+        }
+        else
+        {
+            // All levels completed, restart from level 1
+            currentLevel = 0;
+            ResetGame();
+        }
     }
 
     public void Lose()
     {
-
+        hp--; // Reduce HP when the player loses
+        ResetGame();
     }
 }
